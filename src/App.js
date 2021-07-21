6a6058fb54de06c1b367486e0ef3ad55f7777db5
Katherine Wong <katherinewongkw3@gmail.com>
2021-07-20 17:27:05 -0700

    create react app, and new skill object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react'; 
+
+
+class App extends React.Component {
+  state = {
+    skills: [{ skill: "JavaScript", level: 4 }], 
+    newSkill: { skill:'', level: 3 }
+
+}; 
+
+  addSkill = (e) => {
+    // alert("ADD SKILL CLICKED");
+    console.log(e);
+  }
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value 
+  
+    const newSkill = {
+      ...this.state.newSkill, 
+      // whatever was previously existing ^above, then below, adding a new piece of data, using object literal, replace the previously existing value 
+      [ name ]: value 
+  }
+// above is an object literal 
+
+
+    this.setState({ newSkill })
+  } 
+
+  render() {
+    return (
+      <section>
+        <h2>DEV SKILLS</h2>
+        <hr />
+
+        {/* Show the skills in a list */}
+        {this.state.skills.map(s => (
+          <article key={s.skill}>
+            <div>{s.skill}</div> <div>{s.level}</div>
+          </article>
+        ))}
+        <hr />
+
+        {/* Skills form */}
+        <form>
+          <label>
+            <span>SKILL</span>
+            <input name='skill' value={this.state.newSkill.skill} onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            <span>LEVEL</span>
+            <select name='level' value={this.state.newSkill.level} onChange={this.handleChange}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </label>
+          <button onClick={this.addSkill}>ADD SKILL</button>
+        </form>
+      </section>
+    );
 }
 
+}
 export default App;
